2029381e96d101235997b039e65c884c68f40577
PawelJaskierski <s218269@student.uek.krakow.pl>
2021-11-07 09:41:44 +0100

    Zrobione zadanko

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -4,8 +4,16 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        public static void priceList(float price, int numberOfItems)
+        {
+            for(int i = 1; i <= numberOfItems; i++)
+            {
+                Console.WriteLine($"Liczba sztuk: {i} łączna cena: {i * price}zł");
+            }
+        } 
         static void Main(string[] args)
         {
+            priceList(15, 10);
         }
     }
 }
