92b69f0476988d33bffeabcf382a750bbcb19ee4
PawelJaskierski <s218269@student.uek.krakow.pl>
2021-11-07 10:58:24 +0100

    Robi trójkąt

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -4,8 +4,29 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        public static void Triangle(char sign, int lines)
+        {
+            for(int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(sign);
+                }
+                Console.WriteLine();
+            }
+            for (int i = lines; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(sign);
+                }
+                Console.WriteLine();
+            }
+
+        }
         static void Main(string[] args)
         {
+            Triangle('*', 20);
         }
     }
 }
