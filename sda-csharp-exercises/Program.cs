33a6505107e560bb8229b83986abd10cd9d9fecc
PawelJaskierski <s218269@student.uek.krakow.pl>
2021-11-07 14:27:07 +0100

    Chyba tak jak powinno być

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -5,22 +5,24 @@ namespace sda_csharp_exercises
 {
     class Program
     {
-        public static void NoDuplicats(int[] arrayToPrint)
+        
+        public static bool PESELCheker(string PESEL)
         {
-            List<int> numbersToPrint = new List<int>();
-            foreach(int number in arrayToPrint)
+            int[] weightTable = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            int suma = 0;
+            for (int i = 0; i < PESEL.Length-1; i++)
             {
-                if (!numbersToPrint.Contains(number))
-                {
-                    numbersToPrint.Add(number);
-                    Console.Write(number);
-                }
+                suma += (Convert.ToInt32(PESEL[i]) - 48)*weightTable[i];
+
             }
+            suma %= 10;
+
+            return (10 - suma == Convert.ToInt32(PESEL[PESEL.Length - 1])-48 || (suma == 0 && Convert.ToInt32(PESEL[PESEL.Length - 1])-48 == 0));
+
         }
         static void Main(string[] args)
         {
-            int[] testValue = {1,1,2,2,3};
-            NoDuplicats(testValue);
+            Console.WriteLine(PESELCheker("55030101193"));
         }
     }
 }
