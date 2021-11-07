8824684b67f4c9f057b64683cdacafaa963b20c1
PawelJaskierski <s218269@student.uek.krakow.pl>
2021-11-07 13:55:18 +0100

    Może działać

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -1,11 +1,26 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 namespace sda_csharp_exercises
 {
     class Program
     {
+        public static void NoDuplicats(int[] arrayToPrint)
+        {
+            List<int> numbersToPrint = new List<int>();
+            foreach(int number in arrayToPrint)
+            {
+                if (!numbersToPrint.Contains(number))
+                {
+                    numbersToPrint.Add(number);
+                    Console.Write(number);
+                }
+            }
+        }
         static void Main(string[] args)
         {
+            int[] testValue = {1,1,2,2,3};
+            NoDuplicats(testValue);
         }
     }
 }
