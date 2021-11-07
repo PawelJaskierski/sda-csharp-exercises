5a569f734dfce6c673c3f140ccccbf4cd64d7fd9
PawelJaskierski <s218269@student.uek.krakow.pl>
2021-11-07 10:36:48 +0100

    Suma arytmetyczna

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -4,8 +4,19 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        public static void aritarithmeticProgression(int startValue, int steps, int value)
+        {
+            
+            for(int i = 0; i < steps; i++)
+            {
+                Console.WriteLine(startValue);
+                startValue += value;
+
+            }
+        }
         static void Main(string[] args)
         {
+            aritarithmeticProgression(12, 12, -4);
         }
     }
 }
