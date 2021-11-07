b555b73c53897c84cd5458a690f47e29ce874aef
PawelJaskierski <s218269@student.uek.krakow.pl>
2021-11-07 12:49:48 +0100

    Sprawdza

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using System.Text.RegularExpressions;
+
 
 namespace sda_csharp_exercises
 {
     class Program
     {
+        public static bool passwordChecker(string password)
+        {
+            return Regex.IsMatch(password,"[a-z]") && Regex.IsMatch(password, "[A-Z]") && Regex.IsMatch(password,"[&*$#]") && password.Length >=6;
+
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(passwordChecker("Lubie$"));
+            Console.WriteLine(passwordChecker("a"));
+            Console.WriteLine(passwordChecker("Lubie dolary"));
+            Console.WriteLine(passwordChecker("lubie male litery"));
+            Console.WriteLine(passwordChecker("JESTEM ZLY"));
         }
     }
 }
