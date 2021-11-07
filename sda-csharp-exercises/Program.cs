35da5f2aafab9ac085bb5abb57943e2c2e0095b7
PawelJaskierski <s218269@student.uek.krakow.pl>
2021-11-07 10:08:46 +0100

    Zrobione zadanko

--- sda-csharp-exercises/Program.cs
+++ sda-csharp-exercises/Program.cs
@@ -4,8 +4,26 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        public static float Power(float bas, int exponent)
+        {
+            float output = 1;
+            if (exponent >= 0)
+            {
+                for (int i = 0; i<exponent; i++)
+                {
+                    output *= bas;
+                }
+                return output;
+            }
+            else
+            {
+                Console.WriteLine("Use positive number for the exponent");
+                return float.NaN;
+            }
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine(Power(2, 10));
         }
     }
 }
